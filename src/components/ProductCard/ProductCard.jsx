c8d588343b8500ecaf5408eaa830b5ca136bab27
Ashraf khan <minazpathan2094@gmail.com>
2024-06-02 13:43:36 +0530

    btn disabled added

--- src/components/ProductCard/ProductCard.jsx
+++ src/components/ProductCard/ProductCard.jsx
@@ -58,6 +58,7 @@ const ProductCard = ({ product }) => {
                     })
                   )
                 }
+                disabled={currentProduct?.quantity === product?.quantity}
               >
                 +
               </button>
